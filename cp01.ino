33a7c945b918907fb5fb0541487fc7f8622aff8d
Ruan Gaspar <179716912+RuanGaspar-TDSPA@users.noreply.github.com>
2025-09-06 10:32:04 -0300

    Mapeamento e Setup inicial

--- cp01.ino
+++ cp01.ino
@@ -1,8 +1,40 @@
 // Estrutura básica para a entrega do arquivo
 // Ao concluir, clique na seta SHARE no WOKWI e envie o link como anexo da tarefa.
 
-void setup() {
+// Mapeamento de I/Os (sequência esquerda para direita):
+#include <DHT.h>
 
+#define DHT22 2
+#define DHTTYPE DHT22
+#define Pto A0
+
+#define ldAl 3
+#define ldVo 4
+#define ldAo 5
+#define ldVe 6
+
+#define btA 7
+#define btB 9
+#define btC 11
+#define btD 13
+
+
+
+void setup() {
+pinMode(ldAl, OUTPUT);
+pinMode(ldVo, OUTPUT);
+pinMode(ldAo, OUTPUT);
+pinMode(ldVe, OUTPUT);
+
+pinMode(btA, INPUT_PULLUP);
+pinMode(btB, INPUT_PULLUP);
+pinMode(btC, INPUT_PULLUP);
+pinMode(btD, INPUT_PULLUP);
+
+dht.begin();
+
+Serial.begin(115200);
+Serial.println("Esquema iniciado...");
 }
 
 void loop() {
