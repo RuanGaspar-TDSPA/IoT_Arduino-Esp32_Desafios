6ca02636af1c9777176a2de60cd2297c7e60b44d
Ruan Gaspar <179716912+RuanGaspar-TDSPA@users.noreply.github.com>
2025-09-06 10:04:14 -0300

    Estrutura básica proposta

---
+++ cp01.ino
@@ -0,0 +1,37 @@
+// Estrutura básica para a entrega do arquivo
+// Ao concluir, clique na seta SHARE no WOKWI e envie o link como anexo da tarefa.
+
+void setup() {
+
+}
+
+void loop() {
+
+  ex01();
+  // ex02();
+  // ex03();
+  // ex04();
+  // ex05(); 
+
+}
+
+void ex01(){
+
+}
+
+void ex02(){
+  
+}
+
+void ex03(){
+  
+}
+
+void ex04(){
+  
+}
+
+void ex05(){
+  
+}
+
